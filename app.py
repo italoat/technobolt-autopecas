f44b7917904fc7eebd1f02a289479df1f1fe043e
italoat <49888152+italoat@users.noreply.github.com>
2026-01-08 15:35:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ if not st.session_state.logged_in:
     with col_login:
         st.markdown('<div class="main-card">', unsafe_allow_html=True)
         st.markdown("<h1 class='hero-title'>TECHNOBOLT</h1>", unsafe_allow_html=True)
-        st.markdown("<p style='text-align:center; color:#64748b; margin-bottom:40px;'>REAL ACESSÓRIOS - SISTEMA DE GESTÃO COGNITIVA</p>", unsafe_allow_html=True)
+        st.markdown("<p style='text-align:center; color:#64748b; margin-bottom:40px;'>AUTO PEÇAS - SISTEMA DE GESTÃO COGNITIVA</p>", unsafe_allow_html=True)
         
         user_id = st.text_input("Operador", placeholder="Usuário")
         user_key = st.text_input("Chave", type="password", placeholder="Senha")
